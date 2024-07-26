2c4d618e9abb72ecdfe3ada571be790bd74c1b24
Syahrun Facrezy <68518892+syhrnfcrzy@users.noreply.github.com>
2024-07-26 10:49:01 +0000

    Ubah Gambar

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from streamlit_extras.colored_header import colored_header
 from streamlit_extras.let_it_rain import rain
 
 import nltk
-nltk.download('stopwords')
+
+nltk.download("stopwords")
 from nltk.corpus import stopwords
 
 from nltk.stem import PorterStemmer
@@ -17,14 +18,16 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 import time
 
-st.set_page_config(page_icon="🎭", page_title="KEPUASAN PELANGGAN GOJEK", initial_sidebar_state="auto")
+st.set_page_config(
+    page_icon="🎭", page_title="KEPUASAN PELANGGAN GOJEK", initial_sidebar_state="auto"
+)
 
-#https://blog.gojek.io/content/images/size/w2000/2021/02/API-Design-blog-01-01.png
+# https://blog.gojek.io/content/images/size/w2000/2021/02/API-Design-blog-01-01.png
 page_bg_img = f"""
 <style>
 
 [data-testid="stAppViewContainer"] > .main {{
-background-image: url("LINK_GAMBAR");
+background-image: url("https://blog.gojek.io/content/images/size/w2000/2021/02/API-Design-blog-01-01.png");
 background-size: 100%;
 background-height: auto;
 }}
@@ -83,12 +86,12 @@ st.markdown(m, unsafe_allow_html=True)
 
 # from streamlit_extras.badges import badge
 
-#logo_url = ("https://1000marcas.net/wp-content/uploads/2021/06/Gojek-Logo-2048x1280.png")
+# logo_url = ("https://1000marcas.net/wp-content/uploads/2021/06/Gojek-Logo-2048x1280.png")
 
 
 # badge(type="github", name="gojek", url="https://play.google.com/store/apps/details?id=com.gojek.app")
 
-st.title('Klasifikasi Kepuasan Pelanggan Ojek Online (GOJEK)')
+st.title("Klasifikasi Kepuasan Pelanggan Ojek Online (GOJEK)")
 
 colored_header(
     label=" ",
@@ -96,73 +99,109 @@ colored_header(
     color_name="green-70",
 )
 
-st.write('IMPLEMENTASI TEXT PADA KLASIFIKASI KEPUASAN PELANGGAN TRANSPORTASI ONLINE (OJEK ONLINE) MENGGUNAKAN METODE KNN')
+st.write(
+    "IMPLEMENTASI TEXT PADA KLASIFIKASI KEPUASAN PELANGGAN TRANSPORTASI ONLINE (OJEK ONLINE) MENGGUNAKAN METODE KNN"
+)
 
 # Load the data
 df = pd.read_csv("Data Ulasan Gojek.csv")
 
-df.rename(columns={"userName": "Nama Pengguna", "score": "rating", "at": "tanggal", "content": "ulasan"}, inplace=True)
+df.rename(
+    columns={
+        "userName": "Nama Pengguna",
+        "score": "rating",
+        "at": "tanggal",
+        "content": "ulasan",
+    },
+    inplace=True,
+)
 
 
 # mengganti label pada kolom rating
 def replace_rating(x):
     if x <= 3:
-        return 'Tidak Puas'
+        return "Tidak Puas"
     else:
-        return 'Puas'
+        return "Puas"
 
-df['rating'] = df['rating'].apply(replace_rating)
+
+df["rating"] = df["rating"].apply(replace_rating)
+
 
 # Preprocessing functions
 def preprocess_text(df):
     text = df.lower()
     # Menghapus emotikon
-    emoticon_pattern = re.compile("["
-                                  u"\U0001F600-\U0001F64F"  # emotikon wajah
-                                  u"\U0001F300-\U0001F5FF"  # emotikon kategori simbol
-                                  u"\U0001F680-\U0001F6FF"  # emotikon kategori transportasi dan simbol bisnis
-                                  u"\U0001F1E0-\U0001F1FF"  # emotikon kategori bendera negara
-                                  u"\U00002702-\U000027B0"  # emotikon kategori tanda baca
-                                  u"\U000024C2-\U0001F251"
-                                  "]+", flags=re.UNICODE)
-    text = emoticon_pattern.sub(r'', text)
-    text = re.sub('[%s]' % re.escape(string.punctuation + string.digits), '', text)
+    emoticon_pattern = re.compile(
+        "["
+        "\U0001F600-\U0001F64F"  # emotikon wajah
+        "\U0001F300-\U0001F5FF"  # emotikon kategori simbol
+        "\U0001F680-\U0001F6FF"  # emotikon kategori transportasi dan simbol bisnis
+        "\U0001F1E0-\U0001F1FF"  # emotikon kategori bendera negara
+        "\U00002702-\U000027B0"  # emotikon kategori tanda baca
+        "\U000024C2-\U0001F251"
+        "]+",
+        flags=re.UNICODE,
+    )
+    text = emoticon_pattern.sub(r"", text)
+    text = re.sub("[%s]" % re.escape(string.punctuation + string.digits), "", text)
     text = text.split()
-    stop_words = set(stopwords.words('indonesian'))
+    stop_words = set(stopwords.words("indonesian"))
     text = [word for word in text if word not in stop_words]
     stemmer = PorterStemmer()
     text = [stemmer.stem(word) for word in text]
-    text = ' '.join(text)
+    text = " ".join(text)
     return text
 
 
-
 # Preprocess the reviews
-df['proses ulasan'] = df['ulasan'].apply(preprocess_text)
+df["proses ulasan"] = df["ulasan"].apply(preprocess_text)
 
 
 # Feature extraction using TF-IDF
 tfidf = TfidfVectorizer()
-X = tfidf.fit_transform(df['proses ulasan'])
-Y = df['rating']
+X = tfidf.fit_transform(df["proses ulasan"])
+Y = df["rating"]
 
 ## Train-test split
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, Y, test_size=0.3, random_state=42
+)
 
-#tetangga_terdekat = st.slider('K ', value=440, max_value=800, min_value=1, help="k adalah tetangga terdekat")
+# tetangga_terdekat = st.slider('K ', value=440, max_value=800, min_value=1, help="k adalah tetangga terdekat")
 
 # Train the KNN model
 knn = KNeighborsClassifier(n_neighbors=440)
 knn.fit(X_train, y_train)
 
 y_pred = knn.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred) *100
+accuracy = accuracy_score(y_test, y_pred) * 100
 
 # Daftar kata-kata positif dan negatif
-kata_positif = ['senang', 'membantu', 'lancar',
-                'mantap', 'semoga', 'bagus', 'memuaskan', 'memudahkan', 'sangat puas',
-                'bagus', 'keren', 'puas']
-kata_negatif = ['kecewa', 'buruk', 'jelek', 'mahal', 'tidak', 'error', 'bajingan', 'bangsat']
+kata_positif = [
+    "senang",
+    "membantu",
+    "lancar",
+    "mantap",
+    "semoga",
+    "bagus",
+    "memuaskan",
+    "memudahkan",
+    "sangat puas",
+    "bagus",
+    "keren",
+    "puas",
+]
+kata_negatif = [
+    "kecewa",
+    "buruk",
+    "jelek",
+    "mahal",
+    "tidak",
+    "error",
+    "bajingan",
+    "bangsat",
+]
 
 
 # Fungsi untuk mengklasifikasikan berdasarkan kata-kata positif dan negatif
@@ -182,8 +221,12 @@ def predict_satisfaction(review):
     bobot_negatif = 1
 
     # Menghitung jumlah kata positif dan negatif dalam ulasan
-    jumlah_positif = sum(bobot_positif for kata in kata_positif if kata in processed_review)
-    jumlah_negatif = sum(bobot_negatif for kata in kata_negatif if kata in processed_review)
+    jumlah_positif = sum(
+        bobot_positif for kata in kata_positif if kata in processed_review
+    )
+    jumlah_negatif = sum(
+        bobot_negatif for kata in kata_negatif if kata in processed_review
+    )
 
     # Klasifikasi berdasarkan jumlah kata positif dan negatif
 
@@ -197,26 +240,27 @@ def predict_satisfaction(review):
     # #     return 'Tidak Puas'
 
     if jumlah_positif > jumlah_negatif:
-        return 'Puas'
+        return "Puas"
     else:
-        return 'Tidak Puas'
+        return "Tidak Puas"
+
 
 # User input
-user_input = st.text_area('Masukkan ulasan Anda ⬇')
+user_input = st.text_area("Masukkan ulasan Anda ⬇")
 
-if st.button('Klasifikasi'):
+if st.button("Klasifikasi"):
     if user_input:
-        with st.spinner('Lagi Memuat Mohon Bersabar...'):
+        with st.spinner("Lagi Memuat Mohon Bersabar..."):
             time.sleep(5)
         # Make prediction using KNN model
         prediction = predict_satisfaction(user_input)
 
         # Display the predicted label
-        if prediction == 'Puas':
-            st.success('Berdasarkan ulasan yang Anda berikan, Anda merasa PUAS.')
+        if prediction == "Puas":
+            st.success("Berdasarkan ulasan yang Anda berikan, Anda merasa PUAS.")
             st.balloons()
         else:
-            st.warning('Berdasarkan ulasan yang Anda berikan, Anda merasa TIDAK PUAS.')
+            st.warning("Berdasarkan ulasan yang Anda berikan, Anda merasa TIDAK PUAS.")
         # Display accuracy
         st.markdown(f"Nilai Akurasi : {accuracy:.2f}%")
         rain(
